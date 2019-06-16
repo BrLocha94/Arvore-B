c530bd6943585e13a3016e526112715f6b7cd7ed
BrLocha94 <brunolocha2@gmail.com>
2019-06-16 16:32:48 -0300

    E       E         E       EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -644,6 +644,7 @@ int insere(int cod, char *nome, char *categoria, float preco, char *nome_arquivo
 			noInterno->p[0] = buscaNo;
 			noInterno->p[1] = metadados->pont_prox_no_folha_livre;
 			noInterno->chaves[0] = novo_noFolha->pizzas[0]->cod;
+			noInterno->m ++;
 			
 			fseek(fi, 0, SEEK_SET);
 			salva_no_interno(d, noInterno, fi);
