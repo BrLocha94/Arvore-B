f25b9139ad9dd8433378e4dacbd44e11aa7e93d7
BrLocha94 <brunolocha2@gmail.com>
2019-06-20 18:57:40 -0300

    Added debug logic

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -806,8 +806,8 @@ int exclui(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, cha
 		//CASO A FOLHA TENHA MAIS DE D PIZZAS, A REMOÇÃO SERÁ SIMPLES
 		if(noFolha->m > d){
 			
-			printf("\n NO FOLHA ANTES: \n");
-			imprime_no_folha(d, noFolha);
+			//printf("\n NO FOLHA ANTES: \n");
+			//imprime_no_folha(d, noFolha);
 			
 			//REORDENA A FOLHA PARA SUMIR COM A PIZZA QUE POSSUI O COD PASSADO
 			for(int i = pos_chave; i < noFolha->m; i++){
@@ -819,8 +819,8 @@ int exclui(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, cha
 			//DECRESCE O NUMERO DE CHAVES
 			noFolha->m --;
 			
-			printf("\n NO FOLHA DEPOIS: \n");
-			imprime_no_folha(d, noFolha);
+			//printf("\n NO FOLHA DEPOIS: \n");
+			//imprime_no_folha(d, noFolha);
 			
 			//SALVA A NOVA FOLHA
 			fseek(fd, buscaNo, SEEK_SET);
