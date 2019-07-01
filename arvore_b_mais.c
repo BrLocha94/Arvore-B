b20ddc3940d5522152ace5790e4a8793bb45d4c9
BrLocha94 <brunolocha2@gmail.com>
2019-06-30 22:16:08 -0300

    Consertando bugs na remocao

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -924,9 +924,9 @@ int exclui(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, cha
 								vizinho_interno->m = vizinho_interno->m - 1;
 								
 								//ATUALIZAR CHAVE DO PAI
-								if(pai_interno->pont_pai != -1){
-									pai_interno->chaves[pos_interno] = vizinho_interno->chaves[0];
-								}
+								//if(pai_interno->pont_pai != -1){
+								pai_interno->chaves[pos_interno] = vizinho_interno->chaves[0];
+								//}
 								
 								//SALVA OS NOS ATUALIZADOS
 								fseek(fi, pai_interno->p[pos_interno + 1], SEEK_SET);
@@ -1088,9 +1088,9 @@ int exclui(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, cha
 								vizinho_interno->m = vizinho_interno->m - 1;
 								
 								//ATUALIZAR CHAVE DO PAI
-								if(pai_interno->pont_pai != -1){
-									pai_interno->chaves[pos_interno] = noInterno->chaves[0];
-								}
+								//if(pai_interno->pont_pai != -1){
+								pai_interno->chaves[pos_interno] = noInterno->chaves[0];
+								//}
 								
 								//SALVA OS NOS ATUALIZADOS
 								fseek(fi,  pai_interno->p[pos_interno - 1], SEEK_SET);
